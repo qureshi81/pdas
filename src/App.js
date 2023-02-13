94fcc83b55286cde5afcb0badef8d25e1729273e
hashim <hsqworld@hotmail.com>
2023-02-14 03:31:59 +0500

    Image files added to project

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
-import ProfileCard from "./ProfileCard";
+    import ProfileCard from "./ProfileCard";
+    import AlexaImage from './Images/alexa.png'
+    import CortanaImage from './Images/cortana.png'
+    import SiriImage from './Images/siri.png'
 
 function App(){
     return(
         <div>
         <div>Personal Digital Assistants</div>
+        
+        <img src={AlexaImage}/>
+        <img src={CortanaImage}/>
+        <img src={SiriImage}/>
+            
 
         {/** Props object **/}
-        <ProfileCard title="Alexa" handle="@alexa99"/>
+        <ProfileCard tatle="Alexa" handle="@alexa99"/>
         <ProfileCard title="Cortana" handle="@cortana32"/>
         <ProfileCard title="Siri" handle="@siri01"/>
         </div>
